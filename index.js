588052122e31988b1dc219fb57d7e58f51717349
ylhyaena <ylenia.piromalli@gmail.com>
2024-11-19 05:44:25 +0000

    First Version of web app

---
+++ index.js
@@ -0,0 +1,13 @@
+const c = new AudioContext();
+
+
+function main () {
+    const o = c.createOscillator();
+    o.connect(c.destination);
+
+    o.start();
+}
+
+function stop () {
+    c.suspend();
+}
